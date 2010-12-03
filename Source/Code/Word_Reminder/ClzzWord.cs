407490c85c58de46ab2b7c7d52b8813d2b13cf43
vohlevu <vohlevu@b2e465dc-6431-6fa6-450f-2be2ae7ccd23>
2010-12-03 16:26:14 +0000

    dọp dẹp folder Source.

--- Source/Code/Word_Reminder/ClzzWord.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Word_Reminder
-{
-    class ClzzWord
-    {
-        public string Word { get; set; }
-        public string Mean { get; set; }
-        public string Note { get; set; }
-    }
-}
